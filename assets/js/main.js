51af9320d6bc95b64962588084674f03fadb4420
NovelioPI <novelio.p.i@mail.ugm.ac.id>
2023-08-28 09:35:00 +0700

    Add video Thesis and Horizon Detection

--- assets/js/main.js
+++ assets/js/main.js
@@ -210,7 +210,7 @@
     loop: true,
     autoplay: {
       delay: 5000,
-      disableOnInteraction: false
+      disableOnInteraction: true
     },
     pagination: {
       el: '.swiper-pagination',
